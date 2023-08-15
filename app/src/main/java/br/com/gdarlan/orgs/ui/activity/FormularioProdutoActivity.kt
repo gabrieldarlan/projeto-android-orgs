54f43c43f4ed5e6d8a5eb5e83dd5180167234a47
Gabriel Darlan <gabriel.darlan88@gmail.com>
2023-08-14 21:12:49 -0300

    seção 5 completa

---
+++ app/src/main/java/br/com/gdarlan/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,9 @@
+package br.com.gdarlan.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.gdarlan.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+    
+}
--- app/src/main/java/br/com/gdarlan/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/gdarlan/orgs/ui/activity/MainActivity.kt
@@ -1,17 +1,16 @@
 package br.com.gdarlan.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.gdarlan.orgs.R
 import br.com.gdarlan.orgs.model.Produto
 import br.com.gdarlan.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(
