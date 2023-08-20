7271e04ad8980c42f021cf58bb827bbcb5f1b04e
Gabriel Darlan <gabriel.darlan88@gmail.com>
2023-08-20 15:11:47 -0300

    capitulo completo

--- app/src/main/java/br/com/gdarlan/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/br/com/gdarlan/orgs/dao/ProdutosDao.kt
@@ -1,6 +1,7 @@
 package br.com.gdarlan.orgs.dao
 
 import br.com.gdarlan.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
@@ -13,6 +14,12 @@ class ProdutosDao {
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada de frutas",
+                descricao = "Laranja, maçãs e uvas",
+                valor = BigDecimal("10.90")
+            )
+        )
     }
 }
